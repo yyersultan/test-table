345d87aa198e4d2e9a050cb21466b4bf32652ff6
yersultan <180103230@stu.sdu.edu.kz>
2021-11-12 14:54:54 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { TableData } from "./components/TableData";
+import classes from './App.module.css';
+import { Route } from "react-router-dom";
+import { UserPage } from "./components/User";
+import { LinearProgress } from "@mui/material";
 
 function App() {
+  const[state,setState] = useState({
+    users : [],
+    selected : [],
+    loading : true,
+    error : null,
+    page : 0,
+    sort : 'asc'
+  })
+  useEffect(() => {
+    const getUsers = async() => {
+      try{
+        const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+        console.log(response.data);
+        setState({
+          ...state,
+          users : response.data,
+          loading : false
+        })
+      }catch(e){
+        setState({
+          ...state,
+          error : e.message
+        })
+      }
+    }
+    getUsers();
+    // eslint-disable-next-line
+  },[]);
+
+  const setPage = (page) => {
+    setState({
+      ...state,
+      page
+    })
+  }
+
+  const onDeleteItem = (id) => {
+    setState({
+      ...state,
+      users : state.users.filter(el => el.id !== id)
+    });
+  }
+
+  const sortById = () => {
+    if(state.sort === 'asc'){
+      setState({
+        ...state,
+        users : state.users.sort((a,b) => parseInt(b.id) - parseInt(a.id)),
+        sort : 'desc'
+      })
+    }else{
+      setState({
+        ...state,
+        users : state.users.sort((a,b) => parseInt(a.id) - parseInt(b.id)),
+        sort : 'asc'
+      })
+    }
+    
+  }
+
+  const onUserSelected = (checked,id) =>{
+    if(checked){
+      setState({
+        ...state,
+        selected : [...state.selected,id]
+      })
+    }else{
+      
+      if(state.selected.length > 1){
+        
+        setState({
+          ...state,
+          selected : state.selected.filter(el => el !== id)
+        })
+      }
+    }
+  }
+
+  if(state.loading){
+    return <LinearProgress />
+  }
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = {classes.App}>
+      
+        <Route exact path = "/" render = {
+          () => <TableData 
+                  sortById = {sortById}
+                  setPage = {setPage}
+                  page = {state.page}
+                  sortBy = {state.sort}
+                  selected = {state.selected}
+                  onUserSelected = {onUserSelected}
+                  onDeleteItem = {onDeleteItem} 
+                  users = {state.users}/>}/>
+        
+       
+        <Route path = "/user/:id" render = {
+          () => <UserPage />
+        }/>
+      
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+const app = (
+  <BrowserRouter>
+    <App/>
+  </BrowserRouter>
+)
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {app}
   </React.StrictMode>,
   document.getElementById('root')
 );
