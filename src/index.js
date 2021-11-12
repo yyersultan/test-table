1403f8ada450e8b93e57060eb9188e8671d7baf7
yersultan <180103230@stu.sdu.edu.kz>
2021-11-12 15:07:14 +0600

    done

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 
 const app = (
-  <BrowserRouter>
+  <HashRouter basename = {'/'}>
     <App/>
-  </BrowserRouter>
+  </HashRouter>
 )
 
 ReactDOM.render(
